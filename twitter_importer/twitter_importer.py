f489c254f48614fa2bf290e89cbec3a5918376ac
YuYang <yang.lights@hotmail.com>
2017-08-22 16:28:30 +0800

    init

---
+++ twitter_importer/twitter_importer.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, print_function
+import os
+import mysql.connector
+from mysql.connector import errorcode
+from time import sleep
+
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+# List of the interested topics
+KEYWORDS_LIST = []
+KEYWORDS_LIST += filter(
+    bool, os.environ.get('KEYWORDS_LIST', '').split(','))
+
+#Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+
+# Kafka Configurations
+MYSQL_HOST_NAME = os.environ.get('MYSQL_HOST_NAME')
+MYSQL_ROOT_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
+
+config = {
+    'user': 'root',
+    'password': MYSQL_ROOT_PASSWORD,
+    'host': MYSQL_HOST_NAME,
+    'database': 'twee'
+}
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print (str(data))
+        cnx = cur = None
+        try:
+            cnx = mysql.connector.connect(**config)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print('Something is wrong with your user name or password')
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            cur = cnx.cursor()
+            cur.execute('show databases;')
+            for row in cur.fetchall():
+                print(row)
+        finally:
+            if cur:
+                cur.close()
+            if cnx:
+                cnx.close()
+        return True
+
+    def on_error(self,status):
+        print(status)
+
+def main():
+    l = StdOutListener()
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, l)
+    stream.filter(track=KEYWORDS_LIST)
+
+
+if __name__ == "__main__":
+    main()
